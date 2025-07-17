8d3f445c1b0bda97ffb0a6676dddd066d77f7d3d
Euclid Skyline <euclid.skyline@gmail.com>
2025-07-17 19:30:11 +0300

    Refactor validationLayers and deviceExtensions initialization
    Removed unnecessary line breaks and comments in the
    initialization of the `validationLayers` and
    `deviceExtensions` vectors in `main.cpp` for improved
    readability and conciseness.

--- main.cpp
+++ main.cpp
@@ -16,11 +16,11 @@ const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
 const std::vector<const char*> validationLayers = {
-    "VK_LAYER_KHRONOS_validation"
+	"VK_LAYER_KHRONOS_validation" // This is the standard validation layer provided by Vulkan
 };
 
 const std::vector<const char*> deviceExtensions = {
-    VK_KHR_SWAPCHAIN_EXTENSION_NAME
+	VK_KHR_SWAPCHAIN_EXTENSION_NAME // -> VK_KHR_swapchain is device extension that allows us to create a swapchain
 };
 
 #ifdef NDEBUG
